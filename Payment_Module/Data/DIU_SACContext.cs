9de948555cb39579d665f05f92b24c919f74ba3c
Kazi Khaledur Rahman <38007910+kazirumi@users.noreply.github.com>
2022-05-29 15:41:04 +0600

    Update DIU_SACContext.cs

--- Payment_Module/Data/DIU_SACContext.cs
+++ Payment_Module/Data/DIU_SACContext.cs
@@ -27,8 +27,7 @@ namespace Payment_Module.DiuSac
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Data Source=COMMVAULT\\COMMVAULT;Initial Catalog=DIU_SAC;User ID=sa;Password=Start777");
+
             }
         }
 
